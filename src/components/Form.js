b80405fc14f380f3d30805fd495bddb30cfe392c
tonniegeetz <tonniegeetz@gmail.com>
2024-05-28 08:21:33 +0300

    Understanding forms and conditional form rendering

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
+
+
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +16,54 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // first name is required
+    if (firstName.length > 0) {
+      const formData = { firstName: firstName, lastName: lastName };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+
+
+
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName}{data.lastName}
+      </div>
+    )
+  })
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First name</label>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <label htmlFor="lastName">Last name</label>
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+        : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
+
+
   );
 }
 
